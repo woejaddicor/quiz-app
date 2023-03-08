ec0d8c2a009f5f99c4e32c96cfccfe86c4484d80
Silvia Neves <siltatiana.sn@gmail.com>
2023-03-08 12:41:19 +0000

    Solve conflicts

---
+++ assets/js/quiz.js
@@ -0,0 +1,60 @@
+let availableQuestions = [];
+let currentQuestion = {};
+let choices = Array.from(document.getElementsByClassName("option-text"));
+let questionCounter = 0;
+
+const getCategory = (e) => {
+  e.preventDefault();
+  displayQuestion(e.target.textContent);
+};
+const counterText = document.getElementById("counter-text");
+const quiz = document.getElementsByClassName("buttonCategory");
+let questionArea = document.getElementById("question-area");
+const question = document.getElementById("question");
+
+async function displayQuestion(category) {
+  const response = await fetch(`http://localhost:3000/questions/${category}`);
+  const cat = await response.json();
+  // console.log(cat);
+  questionArea.classList.remove("hidden");
+
+  let questions = cat;
+  let currentQuestionIndex = Math.floor(Math.random() * cat.length);
+  currentQuestion = questions[currentQuestionIndex];
+  console.log(currentQuestion);
+  question.innerHTML = currentQuestion.question;
+
+  choices.forEach((choice) => {
+    const number = choice.dataset.number;
+    choice.innerText = currentQuestion[number];
+    choice.addEventListener("click", (e) => {
+      // console.log(e.target.dataset.number);
+      // console.log(currentQuestion.answer);
+      if (e.target.dataset.number == currentQuestion.answer) {
+        console.log("correct");
+        questions.splice(currentQuestionIndex, 1);
+        questionCounter++;
+        e.stopImmediatePropagation();
+        // console.log(questionCounter);
+        displayQuestion(category);
+      } else if (e.target.dataset.number != currentQuestion.answer) {
+        console.log("wrong");
+        questions.splice(currentQuestionIndex, 1);
+        questionCounter++;
+        e.stopImmediatePropagation();
+        // console.log(questionCounter);
+        displayQuestion(category);
+      }
+    });
+  });
+  counterText.innerText = questionCounter + "/10";
+}
+
+const categoryButton = document.getElementsByClassName("btn");
+for (let i = 0; i < categoryButton.length; i++) {
+  categoryButton[i].addEventListener("click", getCategory);
+}
+
+// const startButton = document.getElementById("start-button");
+// startButton.addEventListener("click", displayQuestion);
+// module.exports = { displayQuestion, getCategory };
