0fcf6f9aece05292d8bd2af36667f3d4eeac199e
woejaddicor <your@emailadd.com>
2023-03-08 16:38:37 +0000

    edit revisionList to render list on screen

--- revisionList.js
+++ revisionList.js
@@ -1,7 +1,13 @@
 const revisionList = document.querySelector('.revisionList');
 const tipsArray = JSON.parse(localStorage.getItem('tipsArray')) || [];
 
-revisionList.innerHTML = tipsArray.map((list) => {
-	console.log(list);
-	return <li>`${list}`</li>;
+tipsArray.map((list) => {
+	console.log(list.tip);
+	return `<li>${list.tip}</li>`;
 });
+
+// highScores
+// 	.map((score) => {
+// 		return `<li class="high-score">${score.name} - ${score.score}</li>`;
+// 	})
+// 	.join('');
