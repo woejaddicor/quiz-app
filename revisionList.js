c75dbffcb9ca4fd1368d9cc5a1c27d3d26105abc
woejaddicor <your@emailadd.com>
2023-03-08 16:30:12 +0000

    html edit to revision.html

--- revisionList.js
+++ revisionList.js
@@ -5,11 +5,3 @@ revisionList.innerHTML = tipsArray.map((list) => {
 	console.log(list);
 	return <li>`${list}`</li>;
 });
-
-// const highScoreList = document.getElementById('highScore-list');
-// const highScores = JSON.parse(localStorage.getItem('highscores')) || [];
-// highScoreList.innerHTML = highScores
-// 	.map((score) => {
-// 		return `<li class="high-score">${score.name} - ${score.score}</li>`;
-// 	})
-// 	.join('');
