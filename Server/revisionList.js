90873381b1879483ef2e0d9ea35e2cac528579c9
woejaddicor <your@emailadd.com>
2023-03-09 12:28:35 +0000

    Added deletealltips function to revision list

--- Server/revisionList.js
+++ Server/revisionList.js
@@ -1,14 +1,24 @@
-const revisionList = document.querySelector(".revisionList");
-const tipsArray = JSON.parse(localStorage.getItem("tipsArray")) || [];
+const revisionList = document.querySelector('.revisionList');
+const tipsArray = JSON.parse(localStorage.getItem('tipsArray')) || [];
 
 let unique = tipsArray.filter(
-    (tip, index) =>
-    index === tipsArray.findIndex((other) => tip.tip === other.tip)
+	(tip, index) =>
+		index === tipsArray.findIndex((other) => tip.tip === other.tip)
 );
 
 revisionList.innerHTML = unique
-    .map((item) => {
-        return `
+	.map((item) => {
+		return `
         <li class="li-revision">${item.tip}</li> <br>`;
-    })
-    .join("");
+	})
+	.join('');
+
+const deleteAllTips = () => {
+	unique.forEach((element) => {
+		localStorage.removeItem('tipsArray', element);
+	});
+	location.reload();
+};
+
+const deleteButton = document.querySelector('.deleteButton');
+deleteButton.addEventListener('click', deleteAllTips);
